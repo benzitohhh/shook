df0f28a6312a3ca942a5868137156bcd5c3cbd74
Ben Immanuel <immanuel_ben@yahoo.co.uk>
2014-03-11 23:51:43 +0000

    new db dumps, moved htaccess into wordpress, more instructions

--- shook.sh
+++ shook.sh
@@ -1,14 +1,28 @@
+# For local, assumed that http://localhost/shook/wordpress points to the wordpress directory
+
 # Convert prod sql to localhost
-#sed "s/http:\/\/www.shook.fm\/content/http:\/\/localhost\/shook/g" dump-local.sql > temp.sql
+# sed "s/http:\/\/www.shook.fm\/content/http:\/\/localhost\/shook\/wordpress/g" prod.sql > temp.sql
 
 # load sql
-mysql --max_allowed_packet=512M -uroot -proot shookfm_wp01 < temp.sql;
+# mysql --max_allowed_packet=512M -uroot -proot shookfm_wp01 < temp.sql;
 
 # If problem, run this from the mysql commandline:
 set global net_buffer_length=1000000; 
 set global max_allowed_packet=1000000000;
 
-# Now, you should be able to access at: http://localhost/bugz/
+# Now, you should be able to access at: http://localhost/shook/wordpress
 
-# Admin is at http://localhost/bugz/wp-admin
+# Admin is at http://localhost/shook/wordpress/wp-admin
 # user=admin, pass=jezmon22 
+
+# To dump local
+# mysqldump -uroot -proot shookfm_wp01 > dump-local-20140311.sql
+
+# To convert localdump to a prod
+# sed "s/http:\/\/localhost\/shook\/wordpress/http:\/\/www.shook.fm/g" dump-local-ben-20140311.sql > temp.sql
+
+# To convert localdump to a bluehost temp dump
+# sed "s/http:\/\/localhost\/shook\/wordpress/http:\/\/box367.bluehost.com\/~shookf1/g" dump-local-ben-20140311.sql > temp.sql
+
+
+
